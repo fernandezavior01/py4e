d35892a2c4c41fe09d6047d14c55295a902c3804
Leila Avior Fernandez <leilaaviorfernandez@gmail.com>
2020-03-12 14:22:14 +0800

    Comment here

---
+++ Exercise1.py
@@ -0,0 +1,7 @@
+hours = float(input("Enter Hours:"))
+rate = float(input("Enter Rate:"))
+if hours <= 40:
+  pay = hours * rate
+else:
+  pay = ((hours - 40) * (1.5 * rate)) + (40 * rate)
+print(pay)
